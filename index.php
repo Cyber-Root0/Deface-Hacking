f27bdb75fffb263d85d58d807a6833ee5972040c
Cyber_Root0 <70926805+Cyber-Root0@users.noreply.github.com>
2020-12-12 19:31:39 -0300

    Deface Hacking - Fins Educacionais
    Deface Hacking - Fins Educacionais

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="http://nanatsunt-hb.weebly.com/uploads/5/0/5/2/50527235/6923438.png?252"/> 
+<title>-Owned- CYBER-</title>
+
+	<meta charset="utf-8">
+	<link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+	<style type="text/css">
+		body {
+			background-color: black;
+		}
+		h1 {
+			font-family: "Poiret One";
+			color: red;
+			font-size: 36pt;
+			text-shadow: red 0 0 5px;
+		}
+		p {
+			font-family: "Poiret One";
+			color: white;
+			font-size: 16pt;
+			text-shadow: white 0 0 5px;
+		}
+	</style>
+</head>
+
+<center>
+		<h1><div style="text-shadow: 5px 5px 5px #333; color:#DF0101;">Hacked bY CYBER</h1>
+ 
+
+
+
+		<iframe width="0" height="0" src="https://www.youtube.com/watch?v=hoVUP9Fd9rc" frameborder="0" allownetworking=”internal”></iframe>
+	</center>
+
+  <tr>
+<center>
+    <td align="center" valign="top"><p><img src="http://3.bp.blogspot.com/-eBPqGM2wMYI/UwUdZU6lrjI/AAAAAAAAHkA/4X6z4HkCCp0/s1600/PNG+-+Caveira+roxa+neon.png" width="412" height="411" /></p></center>
+
+</body>
+
+<div align="center" style="z-index:9;visibility:visible;"><a href="http://www.totallyfreecursors.com/" target="_blank" class="tfc">
+  </a></div><style>HTML,BODY{cursor: url("http://downloads.totallyfreecursors.com/cursor_files/checker1.ani"), url("http://downloads.totallyfreecursors.com/thumbnails/checker1.gif"), auto;}</style>
+</html>
+
+<body>
+	
+		<center><p>Cada sonho que voc&ecirc; deixa pra tr&aacute;s &eacute; um peda&ccedil;o do seu futuro que deixa de existir</p></center> 
+
+<marquee direction=up <P align="center"><center><p>Cyber Intelligence - Inteligencia Cibernetica </center>  </p> </marquee>
